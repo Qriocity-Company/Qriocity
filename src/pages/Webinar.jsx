fb92d61e3ffb223d1b964c634509e036c9e5eb42
SOMESHTHAPLIYALL <149586000+Qriocity-Company@users.noreply.github.com>
2024-09-13 23:15:44 +0530

    Update Webinar.jsx

--- src/pages/Webinar.jsx
+++ src/pages/Webinar.jsx
@@ -173,6 +173,44 @@ const data2 = [
   },
 ];
 
+const timelineData = [
+  {
+    content: "How to choose your project domain and title",
+    img: "https://cdn.prod.website-files.com/63c5e29f1b5bc83fe0af2489/6424d753f8eb7a9e69c372fc_Gantt%20Chart%20Online%20Software%20Instagantt%20Ideation%202.webp",
+    alt: "Choose Project Domain",
+  },
+  {
+    content:
+      "Step-by-step guidance on how to complete your final year project with ease.",
+    img: "https://t3.ftcdn.net/jpg/00/50/28/04/360_F_50280421_c3QPI4se3DD2dpppDZKWv035EAlQrY7J.jpg",
+    alt: "Step by Step guide",
+  },
+  {
+    content:
+      "How to add Novelty to your project and publish your research paper.",
+    img: "https://assets.myperfectwords.com/blog/research-paper-guide/research-paper-outline/Research-Paper-Outline-MPW-9355.jpg",
+    alt: "Research Paper",
+  },
+  {
+    content:
+      "The perfect project roadmap that impresses both professors and recruiters.",
+    img: "https://hrfibreglass.co.uk/wp-content/uploads/2023/09/road-map.jpg",
+    alt: "Project Roadmap",
+  },
+  {
+    content:
+      "Pro tips for scoring good grades in final year projects without burning out.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN-Av4gw8KIUYvW5MLPU6HggvtfK7LWAYsXA&s",
+    alt: "Good Grades",
+  },
+  {
+    content:
+      "Step-by-step roadmap and plan of action on how to get your dream job.",
+    img: "https://www.admitedge.com/blog/wp-content/uploads/2019/02/New-to-GRE-A-step-by-step-Guide-to-GRE-ETS-Registration.jpg",
+    alt: "Dream Job",
+  },
+];
+
 const scrollToTop = () => {
   window.scrollTo(0, 0);
 };
@@ -495,7 +533,7 @@ const AchivementCard = ({ number, title }) => {
 
 const ContentCard = ({ content }) => {
   return (
-    <div className=" md:max-w-3xl max-w-xs  rounded-xl border border-[#2ACDD0] p-6 flex gap-2 md:gap-10 items-center  mx-auto">
+    <div className=" w-full rounded-xl border border-[#2ACDD0] p-6 flex gap-2 md:gap-10 items-center  mx-auto">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="27"
@@ -604,7 +642,7 @@ const Webinar = () => {
   }
 
   return (
-    <div className=" lg:max-w-[1440px] md:max-w[660px] max-w-[760px]  ">
+    <div className=" w-full  ">
       {showForm && <Modal setShowForm={setShowForm} />}
 
       <div className="relative mt-20">
@@ -942,8 +980,8 @@ const Webinar = () => {
           </div>
 
           {/* Exclusive Offer section*/}
-          <div className="mt-20 bg-[#FEEEE9] h-auto  max-w-full rounded-xl flex flex-col justify-start items-center p-4 mb-10">
-            <h1 className="text-4xl font-bold mt-6 lg:text-[50px]">
+          <div className="mt-20 bg-[#FEEEE9] h-auto w-full rounded-xl flex flex-col justify-center items-center p-4 mb-10">
+            <h1 className="text-4xl font-bold mt-10 lg:text-[50px]">
               Exciting <span className="text-[#F26530]">bonuses</span>
             </h1>
             <div className="mt-10 lg:mt-20 flex flex-wrap justify-center items-center gap-4 lg:gap-5 w-full">
@@ -1097,6 +1135,81 @@ const Webinar = () => {
                   ))}
                 </tbody>
               </table>
+            </div>
+          </div>
+
+          <div className="bg-[#EDEDED] mt-20 flex flex-col justify-start items-center p-4">
+            <h1 className="font-bold lg:text-4xl lg:mt-20 mt-8">
+              What will{" "}
+              <span
+                className="lg:px-8 lg:py-2 px-4 py-1 rounded-2xl font-bold text-white"
+                style={{
+                  background:
+                    "linear-gradient(to right, #FBA154 0%, #F15A29 100%)",
+                  display: "inline-block", // Required to apply transformations
+                  transform: "rotate(-2deg)", // Adjust the degree to control the tilt
+                  transformOrigin: "center", // Optional: adjust the pivot point of the rotation
+                }}
+              >
+                you learn{" "}
+              </span>{" "}
+              in this 2-hour session?
+            </h1>
+
+            <div className="relative flex flex-col justify-center items-center mt-10 gap-10 px-4 sm:px-8 md:px-16">
+              {/* Vertical Line */}
+              <div className="absolute inset-0 flex justify-center">
+                <div className="w-1 h-full bg-gray-300 relative">
+                  {/* Circles on the line */}
+                  <div
+                    className="hidden lg:block absolute w-4 h-4 bg-[#F47338] rounded-full transform -translate-x-1/2"
+                    style={{ top: "0%" }} // Start of the line
+                  ></div>
+                  <div
+                    className="hidden lg:block absolute w-4 h-4 bg-[#F47338] rounded-full transform -translate-x-1/2"
+                    style={{ top: "16%" }}
+                  ></div>
+                  <div
+                    className="hidden lg:block absolute w-4 h-4 bg-[#F47338] rounded-full transform -translate-x-1/2"
+                    style={{ top: "33%" }}
+                  ></div>
+                  <div
+                    className="hidden lg:block absolute w-4 h-4 bg-[#F47338] rounded-full transform -translate-x-1/2"
+                    style={{ top: "50%" }}
+                  ></div>
+                  <div
+                    className="hidden lg:block absolute w-4 h-4 bg-[#F47338] rounded-full transform -translate-x-1/2"
+                    style={{ top: "66%" }}
+                  ></div>
+                  <div
+                    className="hidden lg:block absolute w-4 h-4 bg-[#F47338] rounded-full transform -translate-x-1/2"
+                    style={{ top: "83%" }}
+                  ></div>
+                  <div
+                    className="hidden lg:block absolute w-4 h-4 bg-[#F47338] rounded-full transform -translate-x-1/2"
+                    style={{ top: "100%" }} // End of the line
+                  ></div>
+                </div>
+              </div>
+
+              {timelineData.map((item, index) => (
+                <div
+                  key={index}
+                  data-aos="fade-up"
+                  className={`flex flex-col md:flex-row ${
+                    index % 2 === 0 ? "md:flex-row-reverse" : ""
+                  } justify-center items-center gap-10 md:gap-20 z-40`}
+                >
+                  <img
+                    className="h-[250px] w-full md:w-[250px] object-contain"
+                    src={item.img}
+                    alt={item.alt}
+                  />
+                  <div className="bg-slate-200 p-4 rounded-xl h-[200px] w-full md:w-[200px] lg:w-[250px] font-semibold text-lg flex justify-center items-center">
+                    {item.content}
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
 
