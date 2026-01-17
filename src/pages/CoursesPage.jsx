a357be3a410b61183897a551fca172a9c69cc72c
Qriocity-Company <responses.qriocity@gmail.com>
2026-01-17 17:37:06 +0530

    added the meta code in courses page

--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -19,6 +19,27 @@ const CoursesPage = () => {
             setShowPopup(true);
         }, 5000);
 
+        // Meta Pixel Code
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return; n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+            n.queue = []; t = b.createElement(e); t.async = !0;
+            t.src = v; s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+        // Initialize and Track
+        // eslint-disable-next-line no-undef
+        fbq('init', '6604459609678289');
+        // eslint-disable-next-line no-undef
+        fbq('track', 'PageView');
+        // eslint-disable-next-line no-undef
+        fbq('track', 'Lead');
+
         return () => clearTimeout(timer);
     }, []);
 
@@ -37,6 +58,13 @@ const CoursesPage = () => {
                 <PromoPopup onClose={() => setShowPopup(false)} />
             )}
             <StickyBottomBar />
+
+            {/* Meta Pixel NoScript */}
+            <noscript>
+                <img height="1" width="1" style={{ display: 'none' }}
+                    src="https://www.facebook.com/tr?id=6604459609678289&ev=PageView&noscript=1"
+                />
+            </noscript>
         </div>
     );
 };
